fc9e34f9bc0fa1f755441edb7d1244ccf1ae3ccb
SeoYounSeok <syounseok94@gmail.com>
2022-02-09 22:39:29 +0900

    # 2022-02-09 샘플 페이지 제작

---
+++ docs/nextTag.md
@@ -0,0 +1,19 @@
+#### 간단 태그 정리 - 슬쩍 보고 넘어가자
+
+##### Image
+
+```
+* next/image 의 Image 를 사용하면, img 태그를 사용하는 것보다 최적화를 해준다.
+  빌드타임에는 영향이 없으며, 개발자 도구 확인히 확실히, 크기 및 용량이 줄어있었다.
+```
+
+##### CSS Modules
+
+```
+* xxxx.module.css 이란 이름으로 정해줘야 합니다.
+  (className 의 유일성을 확보해준다.)
+
+* Global Styles
+  pages/_app.js
+  styles/global.css
+```
---
+++ nextjs-blog/pages/posts/first-post.jsx
@@ -0,0 +1,25 @@
+import Link from "next/link";
+import Head from "next/head";
+import Layout from "../../components/layout";
+
+export default function FirstPost() {
+  return (
+    <Layout>
+      <Head>
+        <title>First Post</title>
+        <meta name="author" content="서윤석" />
+        <meta name="description" content="이 페이지는 블로그다" />
+      </Head>
+      <h1>First Post</h1>
+      <h2>
+        <Link href="/">
+          <a>Back to home</a>
+        </Link>
+      </h2>
+    </Layout>
+  );
+}
+
+// Link 는 공통된 부분은 그대로 두고, 필요한 부분만 추가적으로 호출한다. 크롬 개발자 도구, 네트워크 탭
+// Link 로 사용한다면, <HTML> 태그에 백그라운 스타일을 입히면, 이동하더라도 스타일이 유지된다.
+// 그러나, <a> 태그를 사용한다면, 새로운 페이지가 생성되는 느낌을 주며 비교적으로 속도가 느리다. (풀 리플레시가 된다.)
